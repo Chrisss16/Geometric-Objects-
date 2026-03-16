e264da25fea1b69d27426f0b1f4185f93912059b
chris <chris@TUF-Chris>
2026-03-16 15:04:30 -0600

    V1

--- Geometric Objects/src/GeometricObject.java
+++ Geometric Objects/src/GeometricObject.java
@@ -1,3 +1,20 @@
+import java.util.Date;
+
 public class GeometricObject {
 
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+    
+    public GeometricObject() {
+        dateCreated = new Date();
+        color = "White";
+        filled = false;
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        dateCreated = new Date();
+        this.color = color;
+        this.filled = filled;
+    }
 }
